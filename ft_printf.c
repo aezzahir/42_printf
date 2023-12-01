295f11f8ddd3badda929cb18df27c0bf09d65f00
ayb1337 <aero.glowsky@gmail.com>
2023-12-01 19:50:41 +0100

    ayoub

--- ft_printf.c
+++ ft_printf.c
@@ -12,10 +12,7 @@ int print_format(char specifier, va_list ap)
 	else if (specifier == 'p')
         count += print_pointer(va_arg(ap, void *));
     else if (specifier == 'd' || specifier == 'i') 
-	{
-        int n = va_arg(ap, int);
-        count += print_in_base((unsigned long)n, "0123456789", n < 0);
-    } 
+        count += print_in_base((unsigned long)(va_arg(ap, int)), "0123456789", (va_arg(ap, int)) < 0);
 	else if (specifier == 'u')
         count += print_in_base(va_arg(ap, unsigned int), "0123456789", 0);
     else if (specifier == 'x')
@@ -36,12 +33,17 @@ int ft_printf(const char *format, ...)
 
 	count = 0;
     va_start(ap, format);
-	if (!format || !format[0] || !format[1])
-		return (-1);
     while (*format != '\0') 
 	{
         if (*format == '%')
-            count += print_format(*(++format), ap);
+		{
+			if (*(++format) == '\0')
+			{
+				va_end(ap);
+				return (count);
+			}
+            count += print_format(*format, ap);
+		}
         else
             count += write(1, format, 1);
         ++format;
--- main.c
+++ main.c
@@ -4,12 +4,7 @@
 
 int main()
 {
-    ft_printf("Hello, world! %s\n", "This is a test");
-    ft_printf("A: Integer: %d, Unsigned: %u, Hex: %x\n", -42, 42, 42);
-	printf("C: Integer: %d, Unsigned: %u, Hex: %x\n", -42, 42, 42);
-    ft_printf("A: Pointer: %p\n", main);
-	printf("C: Pointer: %p\n", main);
-    ft_printf("A: Percent Sign: %%\n");
-	printf("C: Percent Sign: %%\n");
-    return 0;
+    printf("-->%d\n",ft_printf("gg%+ ng#m5kg"));
+    printf("-->%d\n",printf("gg%+ ng#m5kg"));
+    return (0);
 }
