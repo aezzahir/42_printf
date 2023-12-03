89c23fe72300067b45819106496a0c3b1f6a2787
Ayoub Ezzahiri <aezzahir@e1r10p3.1337.ma>
2023-12-03 12:17:04 +0100

    ft_printf 100%" "

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: aezzahir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 11:39:52 by aezzahir          #+#    #+#             */
-/*   Updated: 2023/12/02 11:42:17 by aezzahir         ###   ########.fr       */
+/*   Updated: 2023/12/03 11:54:14 by aezzahir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	print_format(char specifier, va_list ap)
 {
 	int	count;
+	int	n;
 
 	count = 0;
 	if (specifier == 'c')
@@ -24,16 +25,16 @@ int	print_format(char specifier, va_list ap)
 	else if (specifier == 'p')
 		count += print_pointer(va_arg(ap, void *));
 	else if (specifier == 'd' || specifier == 'i')
-		count += print_in_base((unsigned long)(va_arg(ap, int)),
-				"0123456789", (va_arg(ap, int)) < 0);
+	{
+		n = (va_arg(ap, int));
+		count += print_in_base((unsigned long)n, "0123456789", n < 0);
+	}
 	else if (specifier == 'u')
 		count += print_in_base(va_arg(ap, unsigned int), "0123456789", 0);
 	else if (specifier == 'x')
 		count += print_in_base(va_arg(ap, unsigned int), "0123456789abcdef", 0);
 	else if (specifier == 'X')
 		count += print_in_base(va_arg(ap, unsigned int), "0123456789ABCDEF", 0);
-	else if (specifier == '%')
-		count += print_char('%');
 	else
 		count += write(1, &specifier, 1);
 	return (count);
@@ -46,7 +47,7 @@ int	ft_printf(const char *format, ...)
 
 	count = 0;
 	va_start(ap, format);
-	while (*format != '\0')
+	while (format && *format != '\0')
 	{
 		if (*format == '%')
 		{
